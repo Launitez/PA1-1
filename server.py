8c53d3d0df8ffbe89e5ab3437e55244e4e9df916
Ilaun B <brishaebaldwin@gmail.com>
2025-02-03 21:26:45 -0500

    connection successful, 1 OSerror

---
+++ server.py
@@ -0,0 +1,29 @@
+#Ilaun Baldwin-server implementation
+
+#creating server socket 
+import socket
+HOST = "127.0.0.1"
+PORT = 3185
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #binding socket to port and listening for connections from the client
+    s.bind((HOST, PORT))
+    print(f"Binding successful on {HOST}:{PORT}")
+    #Listening for connections
+    s.listen(1);
+    print(f"Listening on {HOST}:{PORT}...")
+    #accepting connections
+    conn, addr = s.accept()
+    print(f"Connection established with {addr}")
+
+    e = socket.error
+    print(f"Socket error: {e}")
+
+#Exchanging data between client and server
+with conn:
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
